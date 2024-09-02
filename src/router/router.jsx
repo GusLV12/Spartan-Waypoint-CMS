42f68b4e1fefb391be904974f5717752fc6e1566
Gustavo Linares Villegas <guslinaresv@gmail.com>
2024-09-02 11:03:10 -0600

    Fix: loggin

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -6,9 +6,9 @@ export const AppRoute = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
       </Route>
-      <Route path="/login" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
